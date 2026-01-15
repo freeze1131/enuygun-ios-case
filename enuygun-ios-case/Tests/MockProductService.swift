cd41f6486065fc494f361080e280509fda3675ea
Aco <ozenahmetcemil@gmail.com>
2026-01-16 00:07:35 +0300

    test: add unit tests for ProductListViewModel using mock service

---
+++ enuygun-ios-case/Tests/MockProductService.swift
@@ -0,0 +1,8 @@
+//
+//  MockProductService.swift
+//  enuygun-ios-case
+//
+//  Created by Ahmet Ozen on 16.01.2026.
+//
+
+import Foundation
---
+++ enuygun-ios-caseTests/enuygun_ios_caseTests.swift
@@ -0,0 +1,16 @@
+//
+//  enuygun_ios_caseTests.swift
+//  enuygun-ios-caseTests
+//
+//  Created by Ahmet Ozen on 16.01.2026.
+//
+
+import Testing
+
+struct enuygun_ios_caseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
