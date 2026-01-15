f2eac119c64ccf628868e61551996305004ae03a
Aco <ozenahmetcemil@gmail.com>
2026-01-15 16:11:20 +0300

    Initial Commit

---
+++ enuygun-ios-case/enuygun_ios_caseApp.swift
@@ -0,0 +1,17 @@
+//
+//  enuygun_ios_caseApp.swift
+//  enuygun-ios-case
+//
+//  Created by Ahmet Ozen on 15.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct enuygun_ios_caseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
